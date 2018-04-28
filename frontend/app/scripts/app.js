a50f51cbbc1a258af8efb4923564ffe2506cdf78
Michael Wang <MichaelWang@michaels-mbp.dhcp.nd.edu>
2018-04-28 15:58:28 -0400

    added frontend ranking two or more users

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+$(document).ready(function(){
+	$('.tabs').tabs();
+});
 angular.module('recommenderApp', ['ui.router', 'ngDialog', 'ngResource', 'ngSanitize', 'recommenderApp.services'])
 .config(function($httpProvider) {
 	$httpProvider.useApplyAsync(true);
